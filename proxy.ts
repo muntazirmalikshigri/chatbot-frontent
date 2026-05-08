2dec9964e4cf32792f988ec20a0995984cdf9911
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2026-05-08 10:46:20 +0500

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,32 +1,12 @@
-// import { NextRequest, NextResponse } from "next/server";
-
-// export function proxy(request: NextRequest) {
-//   const { pathname } = request.nextUrl;
-//   const hasToken = request.cookies.has("accessToken");
-
-//   if (pathname.startsWith("/dashboard") && !hasToken) {
-//     return NextResponse.redirect(new URL("/login", request.url));
-//   }
-
-//   if ((pathname === "/login" || pathname === "/register") && hasToken) {
-//     return NextResponse.redirect(new URL("/dashboard", request.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ["/dashboard/:path*", "/login", "/register"],
-// };
-
-
-
 
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const hasToken = request.cookies.has("accessToken");
+  
+  // Cookie ya Authorization header dono check karo
+  const hasToken = request.cookies.has("accessToken") || 
+    !!request.headers.get("authorization")
 
   if (pathname === "/" || pathname.startsWith("/chat")) {
     return NextResponse.next();
