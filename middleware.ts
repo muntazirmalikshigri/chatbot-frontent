ae07dab39ff78c9289d791ee5cce97217f3460ae
muntazirmalikshigri <muntazirshigri.12@gmail.com>
2026-05-11 17:39:07 +0500

    updated

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(_request: NextRequest) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/login", "/register", "/dashboard/:path*", "/chat/:path*"],
+};
+
+
+
